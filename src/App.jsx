481bc8505d62c1f2d603f35e2e2afd75ee8e3608
asem210 <gustavoadrianpazos@gmail.com>
2024-06-05 18:37:19 -0500

    fix family fonts bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className=" bg-slate-900">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="font-oswald"
-        >
-          count is {count}
-        </button>
-        <p className=" text-[40px] font-belleza">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p className=" text-[40px] font-oswald">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="font-belleza text-[30px]">Hola mundo</p>
+      <p className="text-[30px]">Hola mundo</p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
   theme: {
     extend: {
-      theme: {
-        extend: {
-          colors: {
-            negro: "#1D3139",
-            naranja: "#E9644F",
-            gris: "#E7E7E7",
-            blanco: "#FFFFFF",
-            negroclaro: "#333333",
-            verdeclaro: "#00ff00",
-            rojo: "ff0000",
-          },
-          fontFamily: {
-            poppins: ["Poppins", "sans-serif"],
-            pacifico: ["Pacifico"],
-            oswald: ["Oswald"],
-            ssp: ["Source Sans Pro"],
-            belleza: ["Belleza"],
-          },
-        },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        pacifico: ["Pacifico"],
+        oswald: ["Oswald"],
+        ssp: ["Source Sans Pro"],
+        belleza: ["Belleza"],
       },
+      colors: {},
     },
   },
   plugins: [],
