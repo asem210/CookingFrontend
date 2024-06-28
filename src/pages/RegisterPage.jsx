8d5597d5222d300d3f8aafbd49d2de865d70bd12
asem210 <gustavoadrianpazos@gmail.com>
2024-06-28 10:47:07 -0500

    register responsive design

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,36 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import images from "../constants/images";
 import { MoreInfo } from "../components/forms";
-import { IoArrowBackCircleSharp } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
 import BackToHomeButton from "../components/ReturnTo";
 
 const RegisterPage = () => {
   const getObjectById = (id) => {
     return images.find((item) => item.id === id);
   };
-  const name_proyect = import.meta.env.VITE_NAME_PAGE || "";
 
   const imageObject = getObjectById(6);
   const logo = getObjectById(3);
+
   return (
-    <div className="flex h-screen w-screen">
-      <div className="w-1/2">
-        <img src={imageObject.link} alt="logo" className="w-full h-full" />
+    <div className="flex flex-col md:flex-row h-screen w-screen">
+      {/* Columna izquierda con la imagen */}
+      <div className="w-full md:w-1/2 hidden md:block">
+        <img
+          src={imageObject.link}
+          alt="logo"
+          className="w-full h-full object-cover"
+        />
       </div>
-      <div className="w-1/2 pl-[30px] pr-[30px] py-10 flex flex-col">
+      {/* Columna derecha con el contenido */}
+      <div className="w-full md:w-1/2 p-4 md:pl-[40px] md:pr-[40px] md:py-10 flex flex-col">
         <div className="flex justify-between items-center mb-4">
           <BackToHomeButton text={"Volver"} navigateTo={"login"} />
-
-          <img src={logo.link} alt="Cooking logo" className="ml-auto" />
+          <img
+            src={logo.link}
+            alt="Cooking logo"
+            className="ml-auto w-24 md:w-auto"
+          />
         </div>
         <div className="flex flex-col justify-center items-center w-full">
           <div className="flex pt-3">
-            <p className="font-belleza text-[28px] text-negro">
+            <p className="font-belleza text-[28px] text-negro text-center">
               Continúa con tu registro
             </p>
           </div>
-          <div className="flex flex-col pt-2 w-3/4">
+          <div className="flex flex-col pt-2 w-3/4 md:w-2/3">
             <MoreInfo />
           </div>
         </div>
